d3d10877fb3b83d18486abeb46ed671e3532a834
Gabriel Soares <gabrielsoacc@gmail.com>
2025-01-15 01:13:28 -0300

    feat: added glass efect for navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,16 @@
 const DARK_LIGHT_MODE = document.querySelector(".button-dark-light-mode");
-const HEADER = document.querySelector(".main.header");
+const HEADER = document.querySelector(".main-header");
 const body = document.querySelector("body");
 
 DARK_LIGHT_MODE.addEventListener('click', e => {
     if(!body.classList.contains("dark-mode")){
         body.classList.add("dark-mode");
-    } else{
+        HEADER.classList.remove("glass-efect-light");
+        HEADER.classList.add("glass-efect-dark");
+
+    } else {
         body.classList.remove("dark-mode");
+        HEADER.classList.remove("glass-efect-dark");
+        HEADER.classList.add("glass-efect-light");
     }
 });
