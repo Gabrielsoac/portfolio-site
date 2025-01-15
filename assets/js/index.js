5cbf59845f86f0f40cea6eace0b55f60e34f67a8
Gabriel Soares <gabrielsoacc@gmail.com>
2025-01-15 08:23:54 -0300

    feat: added footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,27 @@
 const DARK_LIGHT_MODE = document.querySelector(".button-dark-light-mode");
+const FOOTER = document.querySelector(".main-footer");
 const HEADER = document.querySelector(".main-header");
 const body = document.querySelector("body");
 
 DARK_LIGHT_MODE.addEventListener('click', e => {
     if(!body.classList.contains("dark-mode")){
+        
         body.classList.add("dark-mode");
-        HEADER.classList.remove("glass-efect-light");
-        HEADER.classList.add("glass-efect-dark");
+        
+        HEADER.classList.remove("glass-effect-light");
+        HEADER.classList.add("glass-effect-dark");
+        
+        FOOTER.classList.remove("glass-effect-light");
+        FOOTER.classList.add("glass-effect-dark");
 
     } else {
         body.classList.remove("dark-mode");
-        HEADER.classList.remove("glass-efect-dark");
-        HEADER.classList.add("glass-efect-light");
+        
+        HEADER.classList.remove("glass-effect-dark");
+        HEADER.classList.add("glass-effect-light");
+
+        FOOTER.classList.remove("glass-effect-dark");
+        FOOTER.classList.add("glass-effect-light");
+
     }
 });
