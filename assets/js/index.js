53691bd15a08f982ae38e06a95380d8d31226fec
Gabriel Soares <gabrielsoacc@gmail.com>
2025-01-27 21:27:58 -0300

    feat: photo frame

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,65 +7,76 @@ const body = document.querySelector("body");
 const subtitle = document.querySelectorAll(".sub-title");
 const SUPER_NAME = document.querySelector(".super-name");
 const FOOTER_NAME = document.querySelector(".footer-name");
+const ME_IMAGE = document.querySelector(".image-button");
 
 DARK_LIGHT_MODE.addEventListener('click', e => {
 
     if(!body.classList.contains("dark-mode")){
-        
-        DARK_LIGHT_MODE.src = "./assets/images/light-mode.png";  
-
-        SUPER_NAME.src = "https://readme-typing-svg.herokuapp.com?font=Playfair+Display&size=30&pause=1000&color=AA73DD&center=true&repeat=false&width=435&lines=Gabriel+Soares";
-        FOOTER_NAME.classList.add("footer-name-dark");
-
-        body.classList.add("dark-mode");
-        
-        HEADER.classList.remove("glass-effect-light");
-        HEADER.classList.add("glass-effect-dark");
-        
-        FOOTER.classList.remove("glass-effect-light");
-        FOOTER.classList.add("glass-effect-dark");
-
-        BUTTON_LINK_PORTFOLIO.classList.remove("glass-effect-light");
-        BUTTON_LINK_PORTFOLIO.classList.add("glass-effect-dark");
-
-        BUTTON_LINK_PROJECT.forEach(e => {
-            e.classList.remove("glass-effect-light");
-            e.classList.add("glass-effect-dark");
-        })
-
-        subtitle.forEach(e => {
-            e.classList.remove("sub-title");
-            e.classList.add("sub-title-dark-mode");
-        });
-
+        setDarkMode();
     } else {
-
-        DARK_LIGHT_MODE.src = "./assets/images/night-mode.png"
-
-        SUPER_NAME.src = "https://readme-typing-svg.herokuapp.com?font=Playfair+Display&size=30&pause=1000&color=6E2AAD&center=true&repeat=false&width=435&lines=Gabriel+Soares";
-  
-        body.classList.remove("dark-mode");
-        FOOTER_NAME.classList.remove("footer-name-dark");
-        
-        HEADER.classList.remove("glass-effect-dark");
-        HEADER.classList.add("glass-effect-light");
-
-        FOOTER.classList.remove("glass-effect-dark");
-        FOOTER.classList.add("glass-effect-light");
-
-        BUTTON_LINK_PROJECT.forEach(e => {
-            e.classList.remove("glass-effect-dark");
-            e.classList.add("glass-effect-light");
-        })
-
-        subtitle.forEach(e => {
-            e.classList.remove("sub-title-dark-mode");
-            e.classList.add("sub-title");
-        });
-
-        BUTTON_LINK_PORTFOLIO.classList.remove("glass-effect-dark");
-        BUTTON_LINK_PORTFOLIO.classList.add("glass-effect-light");
-
+        setLightMode();
     }
 }
 );
+
+function setDarkMode(){
+
+    DARK_LIGHT_MODE.src = "./assets/images/light-mode.png"; 
+    
+    ME_IMAGE.classList.add("image-button-dark-mode");
+
+    SUPER_NAME.src = "https://readme-typing-svg.herokuapp.com?font=Playfair+Display&size=30&pause=1000&color=AA73DD&center=true&repeat=false&width=435&lines=Gabriel+Soares";
+    FOOTER_NAME.classList.add("footer-name-dark");
+
+    body.classList.add("dark-mode");
+    
+    HEADER.classList.remove("glass-effect-light");
+    HEADER.classList.add("glass-effect-dark");
+    
+    FOOTER.classList.remove("glass-effect-light");
+    FOOTER.classList.add("glass-effect-dark");
+
+    BUTTON_LINK_PORTFOLIO.classList.remove("glass-effect-light");
+    BUTTON_LINK_PORTFOLIO.classList.add("glass-effect-dark");
+
+    BUTTON_LINK_PROJECT.forEach(e => {
+        e.classList.remove("glass-effect-light");
+        e.classList.add("glass-effect-dark");
+    })
+
+    subtitle.forEach(e => {
+        e.classList.remove("sub-title");
+        e.classList.add("sub-title-dark-mode");
+    });
+}
+
+function setLightMode(){
+
+    DARK_LIGHT_MODE.src = "./assets/images/night-mode.png"
+
+    ME_IMAGE.classList.remove("image-button-dark-mode");
+
+    SUPER_NAME.src = "https://readme-typing-svg.herokuapp.com?font=Playfair+Display&size=30&pause=1000&color=6E2AAD&center=true&repeat=false&width=435&lines=Gabriel+Soares";
+
+    body.classList.remove("dark-mode");
+    FOOTER_NAME.classList.remove("footer-name-dark");
+    
+    HEADER.classList.remove("glass-effect-dark");
+    HEADER.classList.add("glass-effect-light");
+
+    FOOTER.classList.remove("glass-effect-dark");
+    FOOTER.classList.add("glass-effect-light");
+
+    BUTTON_LINK_PROJECT.forEach(e => {
+        e.classList.remove("glass-effect-dark");
+        e.classList.add("glass-effect-light");
+    })
+
+    subtitle.forEach(e => {
+        e.classList.remove("sub-title-dark-mode");
+        e.classList.add("sub-title");
+    });
+
+    BUTTON_LINK_PORTFOLIO.classList.remove("glass-effect-dark");
+    BUTTON_LINK_PORTFOLIO.classList.add("glass-effect-light");
+}
